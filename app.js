9e53bd9a141b3a21aa516361319cc202ff01ee59
adityapandey27 <adityapandey272002@gmail.com>
2025-11-19 10:49:40 +0530

    updated the cors code

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ dotenv.config({
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+    origin: "*",
   })
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
